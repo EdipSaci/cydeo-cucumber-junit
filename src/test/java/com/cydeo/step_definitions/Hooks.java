0aa8fbc9ddc5eeb4a633a3151a60cfc72d1588b3
Edip SACİ <saciedip@gmail.com>
2023-02-15 13:37:43 +0300

    day15 Hooks

---
+++ src/test/java/com/cydeo/step_definitions/Hooks.java
@@ -0,0 +1,43 @@
+package com.cydeo.step_definitions;
+
+import io.cucumber.java.After;
+import io.cucumber.java.AfterStep;
+import io.cucumber.java.Before;
+import io.cucumber.java.BeforeStep;
+
+public class Hooks {
+
+    // In this class we will be able to pass pre- & post- conditions to each scenario
+
+    //import from io.cucumber.java , not from junit
+    @Before(order = 1)
+    public void setupScenario(){
+        System.out.println("=== Setting up browser using cucumber @Before");
+    }
+
+    @Before (value = "@login" , order = 2)
+    public void setupScenarioForLogins(){
+        System.out.println("=== this will only apply to scenarios with @login tag");
+    }
+
+    @Before (value = "@db", order = 0)
+    public void setupForDatabaseScenarios(){
+        System.out.println("=== this will only apply to scenarios with @db tag");
+    }
+
+    @After
+    public void teardownScenario(){
+        System.out.println("=== Closing browser using cucumber @After");
+    }
+
+    @BeforeStep
+    public void setupStep(){
+        System.out.println("----------> applying setup using @BeforeStep");
+    }
+
+    @AfterStep
+    public void afterStep(){
+        System.out.println("----------> applying tearDown using @AfterStep");
+    }
+
+}
